59e9d3cc8c07485f4c96747a15799fcad18a9fa8
shinbokuow <wyfcyx@gmail.com>
2020-07-19 03:27:36 +0800

    update modified opensbi

---
+++ tools/opensbi/build.sh
@@ -0,0 +1,2 @@
+make PLATFORM=kendryte/k210 CROSS_COMPILE=riscv64-unknown-elf- PLATFORM_RISCV_XLEN=64 V=
+\cp /home/shinbokuow/Desktop/book/k210/wrj/opensbi/build/platform/kendryte/k210/firmware/fw_payload.bin /home/shinbokuow/rCoreTutorial/v3/me/rCore-Tutorial/os/built/fw_payload.bin
