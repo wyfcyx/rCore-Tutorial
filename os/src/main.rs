9e55ef6b4c65240126de62f902c99a61759e6dac
shinbokuow <wyfcyx@gmail.com>
2020-07-19 03:25:08 +0800

    try serial interrupt

--- os/src/main.rs
+++ os/src/main.rs
@@ -13,8 +13,34 @@ mod lang_item;
 mod sbi;
 
 #[no_mangle]
-pub extern "C" fn rust_main() -> ! {
-    println!("Hello world!");
-    panic!("end of rust_main!");
+pub extern "C" fn rust_main(hartid: usize, sp: usize) -> ! {
+    println!("Hello world #{}! sp = 0x{:x}", hartid, sp);
+
+    /*
+    let _hart0_m_int_enable: *mut u32 = 0x0c00_2000 as *mut u32;
+    let _hart0_s_int_enable: *mut u32 = 0x0c00_2080 as *mut u32;
+    let _hart1_m_int_enable: *mut u32 = 0x0c00_2100 as *mut u32;
+    let _hart1_s_int_enable: *mut u32 = 0x0c00_2180 as *mut u32;
+    unsafe {
+        for i in 0xa..=0xd {
+            _hart0_m_int_enable.write_volatile(1 << i);
+            _hart0_s_int_enable.write_volatile(1 << i);
+            _hart1_m_int_enable.write_volatile(1 << i);
+            _hart1_s_int_enable.write_volatile(1 << i);
+        }
+    }
+    */
+
+
+
+    loop {
+        let getc = sbi::console_getchar() as i32;
+        if getc != -1 {
+            print!("{}", getc as u8 as char);
+        }
+    }
+
+    loop {}
+    //panic!("end of rust_main!");
 }
 
