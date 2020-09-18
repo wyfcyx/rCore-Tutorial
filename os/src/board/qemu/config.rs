b9f1278444ca0fe9c50aaa8d3fe67e85e55ffa47
Yifan Wu <shinbokuow@163.com>
2020-09-18 09:26:56 +0800

    Anyway it works now.

--- os/src/board/qemu/config.rs
+++ os/src/board/qemu/config.rs
@@ -3,7 +3,7 @@
 pub const BOARD_MEMORY_END_ADDRESS: usize = 0x8800_0000;
 pub const BOARD_KERNEL_HEAP_SIZE: usize = 0x200_0000;
 pub const BOARD_STACK_SIZE: usize = 0x8000;
-pub const BOARD_KERNEL_STACK_SIZE: usize = 0x8_0000;
+pub const BOARD_KERNEL_STACK_SIZE: usize = 0x4000;
 
 pub const MMIO_INTERVALS: &[(usize, usize)] = &[];
 
