6109e5440d9206cac0efd8c13187fbe463bfe6b5
Yifan Wu <shinbokuow@163.com>
2020-10-17 16:35:25 +0800

    put .eh_frame in .data section & println/panic worked on qemu

--- os/src/main.rs
+++ os/src/main.rs
@@ -2,31 +2,37 @@
 #![no_main]
 #![feature(global_asm)]
 #![feature(llvm_asm)]
+#![feature(panic_info_message)]
 
+#[macro_use]
+mod console;
 mod lang_item;
+mod sbi;
 
 global_asm!(include_str!("entry.asm"));
 
-pub fn console_putchar(ch: u8) {
-    let _ret: usize;
-    let arg0: usize = ch as usize;
-    let arg1: usize = 0;
-    let arg2: usize = 0;
-    let which: usize = 1;
-    unsafe {
-        llvm_asm!("ecall"
-             : "={x10}" (_ret)
-             : "{x10}" (arg0), "{x11}" (arg1), "{x12}" (arg2), "{x17}" (which)
-             : "memory"
-             : "volatile"
-        );
+fn clear_bss() {
+    extern "C" {
+        fn sbss_clear();
+        fn ebss_clear();
+    }
+    let bss_start = sbss_clear as usize;
+    let bss_end = ebss_clear as usize;
+    let bss_aligned = bss_end - bss_end % 8;
+    // clear bss section
+    (bss_start..bss_end).step_by(8).for_each(|p| {
+        unsafe { (p as *mut u64).write_volatile(0) }
+    });
+    if bss_aligned < bss_end {
+        (bss_aligned..bss_end).step_by(1).for_each(|p| {
+            unsafe { (p as *mut u8).write_volatile(0) }
+        });
     }
 }
 
 #[no_mangle]
 pub extern "C" fn rust_main() -> ! {
-    console_putchar(b'O');
-    console_putchar(b'K');
-    console_putchar(b'\n');
-    loop {}
+    clear_bss();
+    println!("Hello rCore-Tutorial!");
+    panic!("end of rust main!");
 }
